4ddd29684485f3574570f0465216c5cee8f52213
Christian Salomon <nomolas.sirhc@gmail.com>
2015-10-31 10:01:47 -0600

    Actualizando la información

--- blink.cpp
+++ blink.cpp
@@ -1,4 +1,12 @@
-/* Equipo AC */
+/* Equipo AC
+	authors:	José Bueno
+            		Christian Alvarez
+
+	date:		30/octubre/2015
+
+	Description:	This program serves as the first practice for arduino
+			and the set up of the enviroment.
+ */
 
 /*
 	aquí va el código
